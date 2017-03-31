98f067467562e5ed7a75068794069ce7d6ddb602
CamVanNguyen <cam.van.nguyen12@gmail.com>
2017-03-31 18:27:13 -0500

    inserting things

---
+++ dbDML.sql
@@ -0,0 +1,11 @@
+INSERT into ITEM (item_id, title, publish_date, recommended_id, user_id, publisher_id, genre_id)
+VALUES (985612903, "Carrie", 1974, 985612904, NULL, 190234789, 201034098);
+INSERT into ITEM (item_id, title, publish_date, recommended_id, user_id, publisher_id, genre_id)
+VALUES (985612904, "End of Watch", 2011, 985612903, NULL, 190234789, 201034098);
+INSERT into ITEM (item_id, title, publish_date, recommended_id, user_id, publisher_id, genre_id)
+VALUES (985612888, "Jurassic Park", 1990, 985612910, NULL, 985612889, 201034100);
+INSERT into ITEM (item_id, title, publish_date, recommended_id, user_id, publisher_id, genre_id)
+VALUES (985612889, "Jurassic Park", 2013, 985612910, NULL, 985612800, 201034100);
+INSERT into ITEM (item_id, title, publish_date, recommended_id, user_id, publisher_id, genre_id)
+VALUES (985612890, "Jurassic Park", 2000, 985612910, NULL, 985612800, 201034100);
+
