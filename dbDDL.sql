305c9a40f47c0f139012f4dee6992ef66f5fa4c0
Cvanguyen <cam.van.nguyen12@gmail.com>
2017-04-02 12:40:23 -0500

    adding the item/media length contraint of 9 digits to where it applies

--- dbDDL.sql
+++ dbDDL.sql
@@ -1,11 +1,11 @@
 create schema Library_Catalog;
 
 CREATE table ITEM( 
-    item_id INT NOT NULL CHECK( item_id <= 99999999 ),
+    item_id INT NOT NULL CHECK( item_id <= 999999999 ),
     title VARCHAR(50) NOT NULL,
     publish_date INT NOT NULL,
     recommened_id NOT NULL CHECK (recommened_id <= 999999999),
-    user_id INT,
+    user_id INT CHECK(user_id <= 9999999999),
     publisher_id INT NOT NULL,
     genre_id INT NOT NULL
     PRIMARY KEY(item_id),
@@ -20,13 +20,13 @@ CREATE table PERSON(
     PRIMARY KEY(person_id)
 );
 CREATE table LIBUSERd
-    user_id INT NOT NULL,
+    user_id INT NOT NULL CHECK (user_id <= 9999999999),
     name VARCHAR(30) NOT NULL,
     address VARCHAR(40) NOT NULL
     PRIMARY KEY(user_id)
 );
 CREATE table BOOK(
-    book_id INT NOT NULL,
+    book_id INT NOT NULL CHECK (book_id <= 9999999999),
     page_length INT NOT NULL CHECK (page_length > 0),
     PRIMARY KEY(book_id),
     FOREIGN KEY(book_id) REFERENCES ITEM(item_id)
@@ -37,43 +37,43 @@ CREATE table GENRE(
     PRIMARY KEY(genre_id)
 );
 CREATE table MEDIA(
-    media_id INT NOT NULL,
+    media_id INT NOT NULL CHECK (media_id <= 999999999),
     runtime INT NOT NULL CHECK(runtime > 0),
     PRIMARY KEY(media_id),
     FOREIGN KEY(media_id) REFERENCES ITEM(item_id)
 );
 CREATE table CD(
-    cd_id INT NOT NULL,
+    cd_id INT NOT NULL CHECK (cd_id <= 999999999), --CD must have 9 digit id and also needs to be not NULL
     num_of_tracks INT NOT NULL CHECK(num_of_track > 0),
     PRIMARY KEY(cd_id),
     FOREIGN KEY(cd_id) REFERENCES MEDIA(media_id)
 );
 CREATE table DVD(
-    dvd_id INT NOT NULL,
+    dvd_id INT NOT NULL CHECK (dvd_id <= 999999999),
     PRIMARY KEY(dvd_id),
     FOREIGN KEY(dvd_id) REFERENCES MEDIA(media_id)
 );
 CREATE table BLURAY(
-    bluray_id INT NOT NULL,
+    bluray_id INT NOT NULL CHECK (bluray_id <= 999999999),
     PRIMARY KEY(bluray_id),
     FOREIGN KEY(bluray_id) REFERENCES MEDIA(media_id)
 );
 CREATE table AUTHOR(
-    book_id INT NOT NULL,
+    book_id INT NOT NULL CHECK (book_id <= 999999999),
     author_id INT NOT NULL,
     PRIMARY KEY(book_id,author_id),
     FOREIGN KEY(book_id) REFERENCES BOOK(book_id),
     FOREIGN KEY(author_id) REFERENCES PERSON(person_id)
 );
 CREATE table PRODUCERDIRECTOR(
-    media_id INT NOT NULL,
+    media_id INT NOT NULL CHECK (media_id <= 999999999),
     pd_id INT NOT NULL,
     PRIMARY KEY(media_id,pd_id),
     FOREIGN KEY(media_id) REFERENCES MEDIA(media_id),
     FOREIGN KEY(pd_id) REFERENCES PERSON(person_id)
 );
 CREATE table ARTIST(
-    media_id INT NOT NULL,
+    media_id INT NOT NULL (media_id <= 999999999),
     artist_id INT NOT NULL,
     PRIMARY KEY(media_id,artist_id),
     FOREIGN KEY(media_id) REFERENCES MEDIA(media_id),
@@ -81,8 +81,8 @@ CREATE table ARTIST(
 );
 CREATE table WRITESREVIEW(
     review_num INT NOT NULL,
-    item_id INT NOT NULL,
-    user_id INT NOT NULL,
+    item_id INT NOT NULL CHECK(item_id <= 999999999),
+    user_id INT NOT NULL CHECK(user_id <= 9999999999),
     PRIMARY KEY(review_num, item_id, user_id),
     FOREIGN KEY(item_id) REFERENCES ITEM(item_id),
     FOREIGN KEY(user_id) REFERENCES LIBUSER(user_id),
@@ -92,15 +92,15 @@ CREATE table REVIEW(
     review_num INT NOT NULL,
     date_review date NOT NULL,
     text VARCHAR(150) NOT NULL,
-    item_id INT NOT NULL,
-    user_id INT NOT NULL,
+    item_id INT NOT NULL CHECK(item_id <= 999999999),
+    user_id INT NOT NULL CHECK(user_id <= 9999999999),
     PRIMARY KEY(item_id,user_id,review_num),
     FOREIGN KEY(item_id) REFERENCES ITEM(item_id),
     FOREIGN KEY(user_id) REFERENCES LIBUSER(user_id)
 );
 CREATE table RATES(
-    item_id INT NOT NULL,
-    user_id INT NOT NULL,
+    item_id INT NOT NULL CHECK(item_id <= 999999999),
+    user_id INT NOT NULL CHECK(user_id <= 9999999999),
     rating INT NOT NULL rating BETWEEN 1 AND 5,
     PRIMARY KEY(item_id, user_id) ,
     FOREIGN KEY(user_id) REFERENCES LIBUSER(user_id),
